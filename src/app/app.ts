347874e162546952842c2fb9a759629061ff258e
UeberTimei <super.timati04@gmail.com>
2025-08-02 21:17:22 +0300

    feat: added basic configuration of the app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherCard } from './components/weather-card/weather-card';
+import { Weather } from './components/weather/weather';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{ title() }}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [Weather],
+  template: ` <app-weather></app-weather> `,
+  styles: [
+    `
+      :host {
+        display: block;
+        font-family: sans-serif;
+        padding: 20px;
+        background-color: #f4f7f9;
+        min-height: 100vh;
+      }
+    `,
+  ],
 })
 export class App {
   protected readonly title = signal('weather-app');
--- src/environments/enviroments.template.ts
+++ src/environments/enviroments.template.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: false,
   openWeatherApiKey: 'YOUR_OPENWEATHER_API_KEY',
   daDataApiKey: 'YOUR_DADATA_API_KEY',
+  daDataSecretKey: 'YOUR_DADATA_SECRET_KEY',
   googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
 };
